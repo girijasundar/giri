59a7cb13bfd79f2bfbb0451dcbb8a48b62139d91
girijasundar <girisundar16@gmail.com>
2016-09-01 18:22:04 +0530

    Create convertint.java

---
+++ convertint.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class convertint
+{
+public static void main(String ar[])
+{
+Scanner s=new Scanner(System.in);
+String h=s.next();
+int f=Integer.parseInt(h);
+System.out.println(f);
+}
+}
